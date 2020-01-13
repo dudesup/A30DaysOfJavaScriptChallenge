cc2f0b80af8f03f6400d5064358adbbd355008bd
Daria Supińska <dariasupinska@gmail.com>
2020-01-13 03:03:15 +0100

    add variables

---
+++ Day1/main.js
@@ -0,0 +1,2 @@
+console.log(firstName, lastName, realName, age, isMarried)
+console.log('pi = ', PI)
---
+++ Day1/variable.js
@@ -0,0 +1,2 @@
+let firstName = 'Henry', lastName = 'Civill', realName = 'Geralt', age = 36, isMarried = false
+const PI = 3.14 //geometrical constant
