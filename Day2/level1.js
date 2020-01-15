d22463a11fd022631dbab131b84a0afc33985b05
Daria Supińska <dariasupinska@gmail.com>
2020-01-15 05:20:31 +0100

    level1 finish

--- Day2/level1.js
+++ Day2/level1.js
@@ -1,1 +1,7 @@
-let challenge = '30 Days Of JavaScript'
+let challenge = '30 Days Of JavaScript' //1
+var challengeArray = challenge.split(' ')
+let company = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+var companyArray = company.split(',')
+let cause = 'You cannot end a sentence with because because because is a conjunction'
+let firstPart = '30 Days of'
+let secondPart = 'JavaScript'
