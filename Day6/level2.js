9b27282a20478451e8ce085f97819835b3e02892
Daria Supińska <dariasupinska@gmail.com>
2020-01-24 02:09:19 +0100

    day6 done

--- Day6/level2.js
+++ Day6/level2.js
@@ -13,6 +13,17 @@ var countries1 = [
   ]
   
 
+  var webTechs1 = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  var mernStack1 = ['MongoDB', 'Express', 'React', 'Node']
 //1
 console.log(Math.random().toString(36))
 
@@ -65,3 +76,26 @@ console.log(iaArr)
 let maxLetter = countries1.map(function(x){return x.length})
 maxLetter.sort()
 console.log(maxLetter[maxLetter.length-1])
+
+//10
+console.log(countries1.filter(country => country.length==5))
+
+//11
+let longestWord = countries1[0]
+for (i=1; i<webTechs1.length-1; i++){
+    if(webTechs1[i].length>longestWord.length){
+        longestWord=webTechs1[i]
+    }
+}
+console.log(longestWord)
+
+//12
+let arrayOfArrays = webTechs1.map(function(element){return [element, element.length]})
+console.log(arrayOfArrays)
+
+//13
+let acronym = ''
+for (i=0; i<mernStack1.length; i++){
+    acronym+=mernStack1[i][0]
+}
+console.log(acronym)
